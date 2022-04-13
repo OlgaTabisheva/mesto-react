9715be750484f76cb37395b8be4a1ca207e761f3
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2022-04-13 22:24:21 +0300

    10 проектная работа - в процессе ревью

--- my-app/src/components/ImagePopup.js
+++ my-app/src/components/ImagePopup.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 
-function ImagePopup({card, onClose}) {
+function ImagePopup(props) {
   return (
-    <div className={`popup popup_type_image-container ${card._id ? 'popup_opened' : ''}`}>
+    <div className={`popup popup_type_image-container ${props.card._id ? 'popup_opened' : ''}`}>
       <div className="popup__container-image">
-        <button type="button" className="popup__close" onClick={onClose}/>
-        <img className="popup__image-link" alt="Изображение места" src={card.link}/>
+        <button type="button" className="popup__close" onClick={props.onClose}/>
+        <img className="popup__image-link" alt="Изображение места" src={props.card.link}/>
         <h2 className="popup__place-name"/>
       </div>
     </div>
